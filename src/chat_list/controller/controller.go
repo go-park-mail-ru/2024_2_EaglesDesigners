c4ac4d3d4e826c5908129eab06156833e576c87c
nonrep <seld210@mail.ru>
2024-09-24 08:12:37 +0300

    fix commit

--- src/chat_list/controller/controller.go
+++ src/chat_list/controller/controller.go
@@ -32,9 +32,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(jsonResp)
+	w.Write(jsonResp)
 }
 
 func init() {
