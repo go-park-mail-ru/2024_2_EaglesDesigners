e90ef3f08878c39dad5696f0b1fb0b87e42f5644
nonrep <seld210@mail.ru>
2024-09-28 16:28:09 +0300

    changed cors

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/go-park-mail-ru/2024_2_EaglesDesigner
 
 go 1.23.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/rs/cors v1.11.1 // indirect
+)
--- src/cors/cors.go
+++ src/cors/cors.go
@@ -1,23 +1,19 @@
 package cors
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/rs/cors"
+)
 
 func CorsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-
-		if origin == "http://localhost:3001" || origin == "http://127.0.0.1:3001" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-
-		next.ServeHTTP(w, r)
+	c := cors.New(cors.Options{
+		AllowedOrigins:     []string{"http://127.0.0.1", "http://localhost", "https://localhost", "http://213.87.134.168"},
+		AllowCredentials:   true,
+		AllowedMethods:     []string{"GET", "POST", "OPTIONS", "DELETE"},
+		AllowedHeaders:     []string{"*"},
+		OptionsPassthrough: false,
 	})
+
+	return c.Handler(next)
 }
