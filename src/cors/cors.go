db0d8e7f2964fe077818bbd29aed3c45727d6a64
nonrep <seld210@mail.ru>
2024-09-28 15:27:44 +0300

    changed cors

--- src/cors/cors.go
+++ src/cors/cors.go
@@ -6,14 +6,13 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
-		if origin == "http://localhost" || origin == "http://127.0.0.1" {
+		if origin == "http://localhost:3001" || origin == "http://127.0.0.1:3001" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		// Обработка preflight-запросов
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
 			return
