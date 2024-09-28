b7854b19d502d366ca399d9e35fdda0447c66494
nonrep <seld210@mail.ru>
2024-09-28 17:03:22 +0300

    changed cors

--- src/cors/cors.go
+++ src/cors/cors.go
@@ -8,7 +8,15 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	c := cors.New(cors.Options{
-		AllowedOrigins:     []string{"http://127.0.0.1", "http://localhost", "https://localhost", "http://213.87.134.168"},
+		AllowedOrigins: []string{
+			"http://127.0.0.1",
+			"https://127.0.0.1",
+			"http://localhost",
+			"https://localhost",
+			"http://213.87.134.168",
+			"https://213.87.134.168",
+			"http://212.233.98.59",
+			"https://212.233.98.59"},
 		AllowCredentials:   true,
 		AllowedMethods:     []string{"GET", "POST", "OPTIONS", "DELETE"},
 		AllowedHeaders:     []string{"*"},
