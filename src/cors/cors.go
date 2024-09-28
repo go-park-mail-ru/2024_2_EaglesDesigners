87d9431e6e6af5ef6d59956f93dd967a705e5648
nonrep <seld210@mail.ru>
2024-09-28 21:42:15 +0300

    fix cors

--- src/cors/cors.go
+++ src/cors/cors.go
@@ -11,14 +11,14 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	log.Println("cors start")
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{
-			"http://127.0.0.1",
-			"https://127.0.0.1",
+			"http://127.0.0.1:8080",
+			"https://127.0.0.1:8080",
 			"http://localhost:8001",
 			"https://localhost:8001",
 			"http://213.87.134.168:8001",
 			"https://213.87.134.168",
-			"http://212.233.98.59",
-			"https://212.233.98.59"},
+			"http://212.233.98.59:8080",
+			"https://212.233.98.59:8080"},
 		AllowCredentials:   true,
 		AllowedMethods:     []string{"GET", "POST", "OPTIONS", "DELETE"},
 		AllowedHeaders:     []string{"*"},
