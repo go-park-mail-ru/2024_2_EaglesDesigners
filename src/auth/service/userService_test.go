26943b11be38c3920bd0d6038faa284411ce4091
nonrep <seld210@mail.ru>
2024-10-02 13:46:38 +0300

    added tokenService tests

--- src/auth/service/userService_test.go
+++ src/auth/service/userService_test.go
@@ -12,26 +12,17 @@ import (
 )
 
 func TestAuthenticate_Success(t *testing.T) {
-
 	mockRepo := &mocks.MockUserRepository{
-
 		GetUserByUsernameFunc: func(username string) (model.User, error) {
-
 			return model.User{
-
 				Username: username,
-
 				Password: utils.HashPassword("pass1"), // Пример правильного хеша
-
 			}, nil
-
 		},
 	}
-
 	authService := service.NewAuthService(mockRepo, nil)
 
 	assert.True(t, authService.Authenticate("user1", "pass1"))
-
 }
 
 func TestAuthenticate_Failure_UserNotFound(t *testing.T) {
@@ -46,90 +37,56 @@ func TestAuthenticate_Failure_UserNotFound(t *testing.T) {
 }
 
 func TestRegistration_Success(t *testing.T) {
-
 	mockRepo := &mocks.MockUserRepository{
-
 		CreateUserFunc: func(username, name, password string) error {
-
 			return nil
-
 		},
 	}
-
 	authService := service.NewAuthService(mockRepo, nil)
-
 	err := authService.Registration("new_user", "John Doe", "pass1")
 
 	assert.NoError(t, err)
-
 }
 
 func TestRegistration_Failure_UserExists(t *testing.T) {
-
 	mockRepo := &mocks.MockUserRepository{
-
 		CreateUserFunc: func(username, name, password string) error {
-
 			return errors.New("user does not exist")
-
 		},
 	}
-
 	authService := service.NewAuthService(mockRepo, nil)
-
 	err := authService.Registration("existing_user", "John Doe", "pass1")
 
 	assert.Error(t, err)
-
 }
 
 func TestGetUserDataByUsername_Success(t *testing.T) {
-
 	mockRepo := &mocks.MockUserRepository{
-
 		GetUserByUsernameFunc: func(username string) (model.User, error) {
-
 			return model.User{
-
-				ID: 1,
-
+				ID:       1,
 				Username: username,
-
-				Name: "John Doe",
+				Name:     "John Doe",
 			}, nil
-
 		},
 	}
-
 	authService := service.NewAuthService(mockRepo, nil)
-
 	userData, err := authService.GetUserDataByUsername("user1")
 
 	assert.NoError(t, err)
-
 	assert.Equal(t, "user1", userData.Username)
-
 	assert.Equal(t, "John Doe", userData.Name)
-
 }
 
 func TestGetUserDataByUsername_Failure_UserNotFound(t *testing.T) {
-
 	mockRepo := &mocks.MockUserRepository{
-
 		GetUserByUsernameFunc: func(username string) (model.User, error) {
-
 			return model.User{}, errors.New("user does not exist")
-
 		},
 	}
-
 	authService := service.NewAuthService(mockRepo, nil)
-
 	userData, err := authService.GetUserDataByUsername("unknown_user")
 
 	assert.Error(t, err)
-
 	assert.Equal(t, utils.UserData{}, userData)
-
 }
