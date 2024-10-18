355f91b9d778a061f8dd1670c693a490f55093a2
nonrep <seld210@mail.ru>
2024-10-18 17:33:35 +0300

    cookie changed

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -63,6 +63,8 @@ func main() {
 
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{
+			"http://localhost:8001",
+			"http://localhost:8080",
 			"http://127.0.0.1:8001",
 			"https://127.0.0.1:8001",
 			"http://localhost:8001",
