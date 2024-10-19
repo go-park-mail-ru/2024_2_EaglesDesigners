3ad114dfd4f7e7d5bf71c84f82f17d9253b54753
nonrep <seld210@mail.ru>
2024-10-19 21:16:24 +0300

    swagger updated

--- docs/docs.go
+++ docs/docs.go
@@ -41,13 +41,13 @@ const docTemplate = `{
                     "200": {
                         "description": "User data retrieved successfully",
                         "schema": {
-                            "$ref": "#/definitions/utils.AuthResponse"
+                            "$ref": "#/definitions/delivery.AuthResponse"
                         }
                     },
                     "401": {
                         "description": "Unauthorized: token is invalid",
                         "schema": {
-                            "$ref": "#/definitions/utils.ErrorResponse"
+                            "$ref": "#/definitions/responser.ErrorResponse"
                         }
                     }
                 }
@@ -81,7 +81,7 @@ const docTemplate = `{
                     "401": {
                         "description": "Unauthorized, no valid access token",
                         "schema": {
-                            "$ref": "#/definitions/controller.ErrorResponse"
+                            "$ref": "#/definitions/delivery.ErrorResponse"
                         }
                     }
                 }
@@ -107,7 +107,7 @@ const docTemplate = `{
                         "in": "body",
                         "required": true,
                         "schema": {
-                            "$ref": "#/definitions/utils.AuthCredentials"
+                            "$ref": "#/definitions/delivery.AuthCredentials"
                         }
                     }
                 ],
@@ -115,19 +115,19 @@ const docTemplate = `{
                     "201": {
                         "description": "Authentication successful",
                         "schema": {
-                            "$ref": "#/definitions/utils.SuccessResponse"
+                            "$ref": "#/definitions/responser.SuccessResponse"
                         }
                     },
                     "400": {
                         "description": "Invalid format JSON",
                         "schema": {
-                            "$ref": "#/definitions/utils.ErrorResponse"
+                            "$ref": "#/definitions/responser.ErrorResponse"
                         }
                     },
                     "401": {
                         "description": "Incorrect login or password",
                         "schema": {
-                            "$ref": "#/definitions/utils.ErrorResponse"
+                            "$ref": "#/definitions/responser.ErrorResponse"
                         }
                     }
                 }
@@ -150,13 +150,13 @@ const docTemplate = `{
                     "200": {
                         "description": "Logout successful",
                         "schema": {
-                            "$ref": "#/definitions/utils.SuccessResponse"
+                            "$ref": "#/definitions/responser.SuccessResponse"
                         }
                     },
                     "401": {
                         "description": "No access token found",
                         "schema": {
-                            "$ref": "#/definitions/utils.ErrorResponse"
+                            "$ref": "#/definitions/responser.ErrorResponse"
                         }
                     }
                 }
@@ -182,7 +182,7 @@ const docTemplate = `{
                         "in": "body",
                         "required": true,
                         "schema": {
-                            "$ref": "#/definitions/utils.RegisterCredentials"
+                            "$ref": "#/definitions/delivery.RegisterCredentials"
                         }
                     }
                 ],
@@ -190,19 +190,19 @@ const docTemplate = `{
                     "201": {
                         "description": "Registration successful",
                         "schema": {
-                            "$ref": "#/definitions/utils.RegisterResponse"
+                            "$ref": "#/definitions/delivery.RegisterResponse"
                         }
                     },
                     "400": {
                         "description": "User failed to create",
                         "schema": {
-                            "$ref": "#/definitions/utils.ErrorResponse"
+                            "$ref": "#/definitions/responser.ErrorResponse"
                         }
                     },
                     "409": {
                         "description": "A user with that username already exists",
                         "schema": {
-                            "$ref": "#/definitions/utils.ErrorResponse"
+                            "$ref": "#/definitions/responser.ErrorResponse"
                         }
                     }
                 }
@@ -210,66 +210,7 @@ const docTemplate = `{
         }
     },
     "definitions": {
-        "controller.ErrorResponse": {
-            "type": "object",
-            "properties": {
-                "error": {
-                    "type": "string"
-                },
-                "status": {
-                    "type": "string",
-                    "example": "error"
-                }
-            }
-        },
-        "model.Chat": {
-            "type": "object",
-            "properties": {
-                "avatarURL": {
-                    "type": "string",
-                    "example": "https://yandex-images.clstorage.net/bVLC53139/667e899dbzgI/Lec3og97oM2J8jgAbwmbs1UEQ_j2WQe6H7Tz0tGHlNUDiLp06xNO9LooehtZCLyucrVfOV3bNS1vNvr_fMoMLbniE8frC6CczUKcwc_ImueU0HKs18lHz490gERWwAOWtD4IttmRuiGPuG9PrfwYeJTUCT5PeyM6mMdYuXvvucreJwTwaprjvy1RSHHf2XlUxVagbjT_Z3s54KP1tiFyt1ZNSQbbE3rzTsqefIsOGIsUYXo-bNgSKZq1WSlJDWhoz9XEo5uL4K6ts3gATet5lVTXMkxWjOY7KIFDVJZywRQU_5pGKzNLwd44T06oTSlJF8LpfEw5cni4VlkKqL3pur7HtGJr6fJM_7N44i2JKwV39mKvRF_0WggAkvaH0qM3dF3rRmhTySM-KU0umyyYWiVw2A7POeGIuvaYeurMKuh91cfSm1uT3gyRmwOOe-pFVEUzzFSupwioEVOHBbBzZ2Tv-6XKcnrQn8kvjEot-5skUlksz1lzKIsGKytJHes6zpeFgbqbE_-fI5hA_TpqVeemkPzm7qcL-UCy5NVyUTYUrfi2K2JIwT85fHzLzVhIFiBIH-9b0Xt45-m5aDwpWu_VtENpCtPcbhE7gL9aisTlNiP8hE2kW1qwEiTkwILXxe9adbgRuvDt6U59ag4qWbVyOm7eG4Nq2QW5mTveSXpcdzTz-TmS_F8iiCEc6Or0JvZy3ldd9-q7MMEWl-FS5YT_GUTZQQqiztuNzust-xr0cJjMzfuSmqqlyvuIPKlJbadlI-l7IS6vUWixLdt6p1dG0dy3bMdYunPBRcWioYYX3BsV2fP7oq26jA6YvgmLxQKqnr_IU0tJJ4greQw5Sp0HhdFYu4DeXGFbEF3JOWb3ZIJvR851WQnhM2UX8VKF983KBAoDOOK9qp1_aC9YK0WCa_8P-UGZ2Eaqy5s9C4qsNofD6Rkg3qyRGCPveNplxMSyzjbctEk5w6C2k"
-                },
-                "chatId": {
-                    "type": "integer",
-                    "example": 1
-                },
-                "chatName": {
-                    "type": "string",
-                    "example": "Чат с пользователем 2"
-                },
-                "chatType": {
-                    "description": "@Enum [personalMessages, group, channel]",
-                    "type": "string",
-                    "example": "personalMessages"
-                },
-                "lastMessage": {
-                    "type": "string",
-                    "example": "Когда за кофе?"
-                },
-                "usersId": {
-                    "type": "array",
-                    "items": {
-                        "type": "integer"
-                    },
-                    "example": [
-                        1,
-                        2
-                    ]
-                }
-            }
-        },
-        "model.ChatsDTO": {
-            "type": "object",
-            "properties": {
-                "chats": {
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/model.Chat"
-                    }
-                }
-            }
-        },
-        "utils.AuthCredentials": {
+        "delivery.AuthCredentials": {
             "type": "object",
             "properties": {
                 "password": {
@@ -282,15 +223,15 @@ const docTemplate = `{
                 }
             }
         },
-        "utils.AuthResponse": {
+        "delivery.AuthResponse": {
             "type": "object",
             "properties": {
                 "user": {
-                    "$ref": "#/definitions/utils.UserData"
+                    "$ref": "#/definitions/delivery.UserData"
                 }
             }
         },
-        "utils.ErrorResponse": {
+        "delivery.ErrorResponse": {
             "type": "object",
             "properties": {
                 "error": {
@@ -302,7 +243,7 @@ const docTemplate = `{
                 }
             }
         },
-        "utils.RegisterCredentials": {
+        "delivery.RegisterCredentials": {
             "type": "object",
             "properties": {
                 "name": {
@@ -319,7 +260,7 @@ const docTemplate = `{
                 }
             }
         },
-        "utils.RegisterResponse": {
+        "delivery.RegisterResponse": {
             "type": "object",
             "properties": {
                 "message": {
@@ -327,24 +268,16 @@ const docTemplate = `{
                     "example": "Registration successful"
                 },
                 "user": {
-                    "$ref": "#/definitions/utils.UserData"
+                    "$ref": "#/definitions/delivery.UserData"
                 }
             }
         },
-        "utils.SuccessResponse": {
-            "type": "object",
-            "properties": {
-                "message": {
-                    "type": "string"
-                }
-            }
-        },
-        "utils.UserData": {
+        "delivery.UserData": {
             "type": "object",
             "properties": {
                 "id": {
-                    "type": "integer",
-                    "example": 2
+                    "type": "string",
+                    "example": "2"
                 },
                 "name": {
                     "type": "string",
@@ -353,6 +286,62 @@ const docTemplate = `{
                 "username": {
                     "type": "string",
                     "example": "user12"
+                }
+            }
+        },
+        "model.ChatDTO": {
+            "type": "object",
+            "properties": {
+                "avatarURL": {
+                    "type": "string",
+                    "example": "https://yandex-images.clstorage.net/bVLC53139/"
+                },
+                "chatName": {
+                    "type": "string",
+                    "example": "Чат с пользователем 2"
+                },
+                "chatType": {
+                    "description": "@Enum [personalMessages, group, channel]",
+                    "type": "string",
+                    "example": "personalMessages"
+                },
+                "countOfUsers": {
+                    "type": "integer"
+                },
+                "lastMessage": {
+                    "type": "string",
+                    "example": "Когда за кофе?"
+                }
+            }
+        },
+        "model.ChatsDTO": {
+            "type": "object",
+            "properties": {
+                "chats": {
+                    "type": "array",
+                    "items": {
+                        "$ref": "#/definitions/model.ChatDTO"
+                    }
+                }
+            }
+        },
+        "responser.ErrorResponse": {
+            "type": "object",
+            "properties": {
+                "error": {
+                    "type": "string"
+                },
+                "status": {
+                    "type": "string",
+                    "example": "error"
+                }
+            }
+        },
+        "responser.SuccessResponse": {
+            "type": "object",
+            "properties": {
+                "message": {
+                    "type": "string"
                 }
             }
         }
