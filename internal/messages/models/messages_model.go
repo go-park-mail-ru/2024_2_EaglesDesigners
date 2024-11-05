ec0e482a5c1c86e939775bacca209f23187d42c9
nonrep <seld210@mail.ru>
2024-11-05 18:20:27 +0300

    added validation to chats and messages

--- internal/messages/models/messages_model.go
+++ internal/messages/models/messages_model.go
@@ -9,13 +9,13 @@ import (
 
 // @Schema
 type Message struct {
-	MessageId  uuid.UUID `json:"messageId" example:"1"`
-	AuthorID   uuid.UUID `json:"authorID" exameple:"2"`
-	AuthorName string    `json:"authorName"`
-	Message    string    `json:"text" example:"тут много текста"`
-	SentAt     time.Time `json:"datetime" example:"2024-04-13T08:30:00Z"`
-	ChatId     uuid.UUID `json:"chatId"`
-	IsRedacted bool      `json:"isRedacted"`
+	MessageId  uuid.UUID `json:"messageId" example:"1" valid:"uuid"`
+	AuthorID   uuid.UUID `json:"authorID" exameple:"2" valid:"uuid"`
+	AuthorName string    `json:"authorName" valid:"matches(^[а-яА-Яa-zA-Z0-9_ ]+$)"`
+	Message    string    `json:"text" example:"тут много текста" valid:"-"`
+	SentAt     time.Time `json:"datetime" example:"2024-04-13T08:30:00Z" valid:"-"`
+	ChatId     uuid.UUID `json:"chatId" valid:"uuid"`
+	IsRedacted bool      `json:"isRedacted" valid:"-"`
 }
 
 func (m Message) MarshalBinary() ([]byte, error) {
@@ -28,22 +28,21 @@ func (m *Message) UnmarshalBinary(data []byte) error {
 }
 
 type MessageInput struct {
-	Message string `json:"text" example:"тут много текста"`
+	Message string `json:"text" example:"тут много текста" valid:"-"`
 }
 
-
 type MessagesArrayDTO struct {
-	Messages []Message `json:"messages"`
+	Messages []Message `json:"messages" valid:"-"`
 }
 
 type MessagesArrayDTOOutput struct {
-	IsNew    bool      `json:"isNew"`
-	Messages []Message `json:"messages"`
+	IsNew    bool      `json:"isNew" valid:"-"`
+	Messages []Message `json:"messages" valid:"-"`
 }
 
 type MessageDTOInput struct {
-	Disconnect bool   `json:"disconnect"`
-	Message    string `json:"message"`
+	Disconnect bool   `json:"disconnect" valid:"-"`
+	Message    string `json:"message" valid:"-"`
 }
 
 type MsgType string
