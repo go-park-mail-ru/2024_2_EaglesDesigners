914de1787ff3856ee2590b97d2f55c0f5bc91ccb
lev <qwerty_268@list.ru>
2024-11-20 20:20:40 +0300

    feat delete one user

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -175,6 +175,7 @@ func main() {
 	router.HandleFunc("/chat/search", auth.Authorize(auth.Csrf(chat.SearchChats))).Methods("GET", "OPTIONS")
 	router.HandleFunc("/chat/{chatId}/addusers", auth.Authorize(auth.Csrf(chat.AddUsersIntoChat))).Methods("POST", "OPTIONS")
 	router.HandleFunc("/chat/{chatId}/delusers", auth.Authorize(auth.Csrf(chat.DeleteUsersFromChat))).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/chat/{chatId}/deluser/{userId}", auth.Authorize(auth.Csrf(chat.DeleteUserFromChat))).Methods("DELETE", "OPTIONS")
 	router.HandleFunc("/chat/{chatId}/delete", auth.Authorize(auth.Csrf(chat.DeleteChatOrGroup))).Methods("DELETE", "OPTIONS")
 	router.HandleFunc("/chat/{chatId}", auth.Authorize(auth.Csrf(chat.UpdateGroup))).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/chat/{chatId}", auth.Authorize(chat.GetChatInfo)).Methods("GET", "OPTIONS")
