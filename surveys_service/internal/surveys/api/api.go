4c21e325e972c76afebb11b9adb76b02fa848d30
nonrep <seld210@mail.ru>
2024-11-23 13:54:47 +0300

    added survey api

--- surveys_service/internal/surveys/api/api.go
+++ surveys_service/internal/surveys/api/api.go
@@ -1,20 +1,31 @@
 package api
 
-import "context"
+import (
+	"context"
+
+	surveysv1 "github.com/go-park-mail-ru/2024_2_EaglesDesigner/surveys_service/internal/proto"
+)
 
 type Surveys interface {
-	GetStatictics(ctx context.Context, in *surveysv1.GetStaticticsReq) (*surveysv1.GetStaticticsResp, error)
+	// GetStatictics(ctx context.Context, in *surveysv1.GetStaticticsReq) (*surveysv1.GetStaticticsResp, error)
 	GetSurvey(ctx context.Context, in *surveysv1.GetSurveyReq) (*surveysv1.GetSurveyResp, error)
-	AddAnswers(ctx context.Context, in *surveysv1.AddAnswersReq) (*surveysv1.AddAnswersResp, error)
+	AddAnswers(ctx context.Context, in *surveysv1.AddAnswersReq) (*surveysv1.Nothing, error)
 }
 
 type Server struct {
-	surveysv1.UnimplementedAuthServer
+	surveysv1.UnimplementedSurveysServer
 	surveys Surveys
 }
 
 func New(surveys Surveys) surveysv1.SurveysServer {
 	return Server{
-		auth: auth,
+		surveys: surveys,
 	}
+}
+
+func (s Server) GetSurvey(ctx context.Context, in *surveysv1.GetSurveyReq) (*surveysv1.GetSurveyResp, error) {
+	return s.surveys.GetSurvey(ctx, in)
+}
+func (s Server) AddAnswers(ctx context.Context, in *surveysv1.AddAnswersReq) (*surveysv1.Nothing, error) {
+	return s.surveys.AddAnswers(ctx, in)
 }
