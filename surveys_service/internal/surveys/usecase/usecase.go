94597c106604834fc740164f0ff7f842187bf063
lev <qwerty_268@list.ru>
2024-11-23 15:50:06 +0300

    feat stat

--- surveys_service/internal/surveys/usecase/usecase.go
+++ surveys_service/internal/surveys/usecase/usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 
 	surveysv1 "github.com/go-park-mail-ru/2024_2_EaglesDesigner/surveys_service/internal/proto"
 	"github.com/go-park-mail-ru/2024_2_EaglesDesigner/surveys_service/internal/surveys/models"
@@ -15,6 +16,7 @@ type ServeyRepository interface {
 	AddAnswer(ctx context.Context, answer models.Answer) error
 	GetAllTextAnswers(ctx context.Context, questionId uuid.UUID) ([]string, error)
 	GetAverageNumericAnswer(ctx context.Context, questionId uuid.UUID) (int, error)
+	GetQuestionType(ctx context.Context, questionId uuid.UUID) (string, error)
 }
 
 type ServeyUsecase struct {
@@ -92,6 +94,45 @@ func (u ServeyUsecase) AddAnswers(ctx context.Context, in *surveysv1.AddAnswersR
 	}, nil
 }
 
+const text = "text"
+const numeric = "numeric"
+
 func (u ServeyUsecase) GetStatictics(ctx context.Context, in *surveysv1.GetStaticticsReq) (*surveysv1.GetStaticticsResp, error) {
-	return nil, nil
+
+	questionId, err := uuid.Parse(in.Question_Id)
+	if err != nil {
+		return nil, err
+	}
+
+	questionType, err := u.repository.GetQuestionType(ctx, questionId)
+	if err != nil {
+		return nil, err
+	}
+
+	switch questionType {
+	case text:
+		questions, err := u.repository.GetAllTextAnswers(ctx, questionId)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resp := surveysv1.GetStaticticsResp{
+			QuestionsAnswerText: questions,
+		}
+		return &resp, nil
+	case numeric:
+		avg, err := u.repository.GetAverageNumericAnswer(ctx, questionId)
+
+		if err != nil {
+			return nil, err
+		}
+
+		resp := surveysv1.GetStaticticsResp{
+			QuestionsAnswerNumeric: int64(avg),
+		}
+		return &resp, nil
+	}
+
+	return nil, fmt.Errorf("ало")
 }
