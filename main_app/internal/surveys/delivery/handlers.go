9c7fb9ce977210d65fb1c76aead185bb69338498
nonrep <seld210@mail.ru>
2024-11-23 14:59:24 +0300

    proto survey fixed

--- main_app/internal/surveys/delivery/handlers.go
+++ main_app/internal/surveys/delivery/handlers.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	"github.com/go-park-mail-ru/2024_2_EaglesDesigner/global_utils/logger"
+	"github.com/go-park-mail-ru/2024_2_EaglesDesigner/main_app/internal/utils/responser"
 	surveyv1 "github.com/go-park-mail-ru/2024_2_EaglesDesigner/protos/gen/go/surveyv1"
+	"github.com/gorilla/mux"
 )
 
 type Delivery struct {
@@ -21,13 +23,31 @@ func (d *Delivery) GetStatictics(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// @Router /survey/{name} [get]
 func (d *Delivery) GetSurvey(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log := logger.LoggerWithCtx(ctx, logger.Log)
 
-	
+	vars := mux.Vars(r)
+	surveyName := vars["name"]
+
+	grpcResp, err := d.client.GetSurvey(
+		ctx,
+		&surveyv1.GetSurveyReq{
+			Name: surveyName,
+		},
+	)
+
+	if err != nil {
+		log.Errorf("не удалось получить опрос")
+		responser.SendError(ctx, w, responser.InvalidJSONError, http.StatusBadRequest)
+	}
+
+	responser.SendStruct(ctx, w, resp, http.StatusOK)
 }
 
 func (d *Delivery) AddAnswers(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func convertFromGRPCSurvey(survey surveyv1.GetSurveyResp) models.
