e8df12ce1cebc9a55da57f573f24dc52a8530cd0
nonrep <seld210@mail.ru>
2024-11-23 13:31:52 +0300

    added surveys proto

--- surveys_service/internal/surveys/api/api.go
+++ surveys_service/internal/surveys/api/api.go
@@ -1,9 +1,11 @@
 package api
 
+import "context"
+
 type Surveys interface {
-	GetStatictics(ctx context.Context, in *surveysv1.) (*surveysv1. ,error)
-	GetSurvey(ctx context.Context, in *surveysv1.) (*surveysv1. ,error) 
-    AddAnswers(ctx context.Context, in *surveysv1.) (*surveysv1. ,error)
+	GetStatictics(ctx context.Context, in *surveysv1.GetStaticticsReq) (*surveysv1.GetStaticticsResp, error)
+	GetSurvey(ctx context.Context, in *surveysv1.GetSurveyReq) (*surveysv1.GetSurveyResp, error)
+	AddAnswers(ctx context.Context, in *surveysv1.AddAnswersReq) (*surveysv1.AddAnswersResp, error)
 }
 
 type Server struct {
@@ -16,6 +18,3 @@ func New(surveys Surveys) surveysv1.SurveysServer {
 		auth: auth,
 	}
 }
-
-
-
