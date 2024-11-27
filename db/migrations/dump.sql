955f1797245eb6b1b2a4e0f50b3287468aeb3d28
qwerty268 <lev-demchenko@bk.ru>
2024-11-27 14:37:26 +0300

    fix db

--- db/migrations/dump.sql
+++ db/migrations/dump.sql
@@ -377,10 +377,6 @@ INSERT INTO chat (chat_name, chat_type_id, id) VALUES
 
 INSERT INTO chat_user (id, user_role_id, chat_id, user_id) VALUES
     ('a0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2, (SELECT id FROM public.chat WHERE chat_name = 'oleg'), (SELECT id FROM public.user where username ='user11')),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     ('b0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2,(SELECT id FROM public.chat WHERE chat_name = 'oleg'),  (SELECT id FROM public.user where username ='user22')),
     ('c0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2,(SELECT id FROM public.chat WHERE chat_name = 'kizaru'), (SELECT id FROM public.user where username ='user11')),
     ('d0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2,(SELECT id FROM public.chat WHERE chat_name = 'kizaru'), (SELECT id FROM public.user where username ='user44')),
@@ -395,34 +391,4 @@ INSERT INTO chat_user (id, user_role_id, chat_id, user_id) VALUES
     ('f8a0aaa0-d461-437d-b4eb-bf030a0efc80', 3,(SELECT id FROM public.chat WHERE chat_name = 'not funny channel'), (SELECT id FROM public.user where username ='user44')),
     ('f9a0aaa0-d461-437d-b4eb-bf030a0efc80', 1,(SELECT id FROM public.chat WHERE chat_name = 'not funny channel'), (SELECT id FROM public.user where username ='user33')),
     ('a1a0aaa0-d461-437d-b4eb-bf030a0efc80', 3,(SELECT id FROM public.chat WHERE chat_name = 'my little channel'), (SELECT id FROM public.user where username ='user44'));
-=======
-=======
->>>>>>> RK3
-=======
->>>>>>> RK3
-=======
->>>>>>> RK3
-    ('b0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2, (SELECT id FROM public.chat WHERE chat_name = 'oleg'),  (SELECT id FROM public.user where username ='user22')),
-    ('c0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2, (SELECT id FROM public.chat WHERE chat_name = 'kizaru'), (SELECT id FROM public.user where username ='user11')),
-    ('z0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2, (SELECT id FROM public.chat WHERE chat_name = 'kizaru'), (SELECT id FROM public.user where username ='user22')),
-    ('e0a0aaa0-d461-437d-b4eb-bf030a0efc80', 2, (SELECT id FROM public.chat WHERE chat_name = 'marsel'), (SELECT id FROM public.user where username ='user11')),
-    ('f0a0aaa0-d461-437d-b4eb-bf030a0efc80', 1, (SELECT id FROM public.chat WHERE chat_name = 'marsel'), (SELECT id FROM public.user where username ='user22')),
-    ('f1a0aaa0-d461-437d-b4eb-bf030a0efc80', 1, (SELECT id FROM public.chat WHERE chat_name = 'marsel'), (SELECT id FROM public.user where username ='user33')),
-    ('f2a0aaa0-d461-437d-b4eb-bf030a0efc80', 3, (SELECT id FROM public.chat WHERE chat_name = 'marsel'), (SELECT id FROM public.user where username ='user44')),
-    ('f4a0aaa0-d461-437d-b4eb-bf030a0efc80', 3, (SELECT id FROM public.chat WHERE chat_name = 'funny channel'), (SELECT id FROM public.user where username ='user11')),
-    ('f5a0aaa0-d461-437d-b4eb-bf030a0efc80', 2, (SELECT id FROM public.chat WHERE chat_name = 'funny channel'), (SELECT id FROM public.user where username ='user22')),
-    ('f6a0aaa0-d461-437d-b4eb-bf030a0efc80', 1, (SELECT id FROM public.chat WHERE chat_name = 'funny channel'), (SELECT id FROM public.user where username ='user33')),
-    ('f7a0aaa0-d461-437d-b4eb-bf030a0efc80', 1, (SELECT id FROM public.chat WHERE chat_name = 'not funny channel'), (SELECT id FROM public.user where username ='user22')),
-    ('f8a0aaa0-d461-437d-b4eb-bf030a0efc80', 3, (SELECT id FROM public.chat WHERE chat_name = 'not funny channel'), (SELECT id FROM public.user where username ='user44')),
-    ('f9a0aaa0-d461-437d-b4eb-bf030a0efc80', 1, (SELECT id FROM public.chat WHERE chat_name = 'not funny channel'), (SELECT id FROM public.user where username ='user33')),
-    ('a1a0aaa0-d461-437d-b4eb-bf030a0efc80', 3, (SELECT id FROM public.chat WHERE chat_name = 'my little channel'), (SELECT id FROM public.user where username ='user44'));
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> ce95e8774339bd426fac838aa7605c88ead9f29e
-=======
->>>>>>> RK3
-=======
->>>>>>> RK3
-=======
->>>>>>> RK3
+
